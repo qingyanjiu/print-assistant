af5bf48678ad5726096f1c53238fdaefe9b11f3e
louisliu <louisliu@cisco.com>
2019-10-10 14:29:39 +0800

    added expire date for history

--- src/main/java/site/moku/printassistant/service/PrintInfoService.java
+++ src/main/java/site/moku/printassistant/service/PrintInfoService.java
@@ -5,20 +5,25 @@
 package site.moku.printassistant.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class PrintInfoService implements IPrintInfoService {
+
+    @Value("${print.history.expire.days}")
+    private long expireDays;
 
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
 
     @Override
     public void saveToRedis(String printKey, String content) {
-        stringRedisTemplate.opsForValue().set(printKey, content);
+        stringRedisTemplate.opsForValue().set(printKey, content, expireDays, TimeUnit.DAYS);
     }
 
     @Override
