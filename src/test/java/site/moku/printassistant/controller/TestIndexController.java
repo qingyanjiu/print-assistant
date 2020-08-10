f7ce978e7acd656a7db32fa003ef3e9075a3ece4
Louis <qingyanjiu@126.com>
2020-08-10 22:25:04 +0800

    added unit test

---
+++ src/test/java/site/moku/printassistant/controller/TestIndexController.java
@@ -0,0 +1,30 @@
+package site.moku.printassistant.controller;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestIndexController {
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() throws Exception {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+}
---
+++ src/test/java/site/moku/printassistant/service/TestPrintInfoService.java
@@ -0,0 +1,36 @@
+package site.moku.printassistant.service;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestPrintInfoService {
+
+    @MockBean
+    private IPrintInfoService printInfoService;
+
+
+    @Before
+    public void setUp() throws Exception {
+        when(printInfoService.getHistoryFromRedis()).thenReturn(null);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testGetHistoryFromRedis() {
+        Assert.assertNull(printInfoService.getHistoryFromRedis());
+    }
+}
