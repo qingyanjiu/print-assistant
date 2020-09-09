7c7c5f5b3c5ae6513bde5c3fd1fa0347412cca61
Louis <qingyanjiu@126.com>
2020-09-09 20:42:07 +0800

    update

--- src/main/java/site/moku/tools/ChangeName.java
+++ src/main/java/site/moku/tools/ChangeName.java
@@ -5,16 +5,13 @@ import java.util.Arrays;
 
 public class ChangeName {
     public static void main(String[] args) {
-        String path = "E:\\games\\board-game\\Splendor The Cities of Splendor";
+        String path = "C:\\Users\\Louis\\Desktop\\w";
         File file = new File(path);
         if(file.isDirectory()) {
             File[] files = file.listFiles();
             Arrays.asList(files).forEach(item->{
-                if(item.getName().contains(".png")) {
-                    item.renameTo(new File(item.getAbsolutePath().replace(".png",".jpg")));
-                }
-                else if(!item.getName().contains(".")) {
-                    item.renameTo(new File(item.getAbsolutePath()+".png"));
+                if(item.getName().contains("-")) {
+                    item.renameTo(new File(item.getAbsolutePath().replace("-","_")));
                 }
             });
         }
