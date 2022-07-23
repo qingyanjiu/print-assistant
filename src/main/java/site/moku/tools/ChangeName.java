60405e730b2bf40209cd8f5a2255b15edd373c65
louis <qingyanjiu@hotmail.com>
2022-07-23 10:15:33 +0800

    修改文件名字

--- src/main/java/site/moku/tools/ChangeName.java
+++ src/main/java/site/moku/tools/ChangeName.java
@@ -5,13 +5,15 @@ import java.util.Arrays;
 
 public class ChangeName {
     public static void main(String[] args) {
-        String path = "C:\\Users\\Louis\\Desktop\\w";
+        String path = args[0];
+        String origin = args[1];
+        String dist = args[2];
         File file = new File(path);
         if(file.isDirectory()) {
             File[] files = file.listFiles();
             Arrays.asList(files).forEach(item->{
-                if(item.getName().contains("-")) {
-                    item.renameTo(new File(item.getAbsolutePath().replace("-","_")));
+                if(item.getName().contains("20120601")) {
+                    item.renameTo(new File(item.getAbsolutePath().replace(origin, dist)));
                 }
             });
         }
